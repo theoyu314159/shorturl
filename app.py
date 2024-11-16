2e73ae98417fc5dcbea1df963d755b98ba2466ca
theoyu314159 <125031865+theoyu314159@users.noreply.github.com>
2024-11-16 23:18:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import request
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    html=''
+    with open('index.html',"r",encoding="utf-8") as f:
+        html = f.read()
+        print('a')
+    return html
+
+@app.route('/url_shorten', methods=['POST'])
+def submit():
+    html=''
+    submit = request.form.get('url')
+    with open('index.html',"r",encoding="utf-8") as f:
+        html = f.read()
+        print('a')
+    with open('data.txt',"a",encoding="utf-8") as f:
+        f.write(submit+ "\n")
+    with open('data.txt', "r", encoding="utf-8") as f:
+        urls = f.read()
+    urlarr=urls.split("\n")
+    k=len(urlarr)
+    print(k)
+    return html.replace('<out/>',f'http://192.168.1.105:5000/{k-1}')
+
+@app.route('/<string:inp>')
+def do(inp):
+    with open('data.txt', "r", encoding="utf-8") as f:
+        urls = f.read()
+    urlarr=urls.split("\n")
+    out=int(inp)-1
+    return  redirect(urlarr[out])
+
+app.run(debug=True,  host='0.0.0.0')
